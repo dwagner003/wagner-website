2235afafdd947a1a903f16a4ddf180ef1b8b0859
Devin Wagner <devinwagner003@gmail.com>
2020-05-11 21:14:14 -0600

    Adding prod env file, dependencies, and script for Heroku hosting.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  auth: {
+    clientID: 'fW9r7EF5eQQ8iJCtkxQKzPDErVFtM3uL',
+    domain: 'dev-85d746sq.auth0.com', // e.g., https://you.auth0.com/
+    audience: 'http://localhost:4200', // e.g., http://localhost:3001
+    redirect: 'http://localhost:4200/callback',
+    scope: 'openid profile email'
+  }
 };
