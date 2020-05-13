63a339dbdef1d7d7a1f5ce6cd1a4f2c8ef83c0c5
Devin Wagner <devinwagner003@gmail.com>
2020-05-12 21:52:13 -0600

    Correcting spelling error for the redirect url in the prod env file.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     clientID: 'fW9r7EF5eQQ8iJCtkxQKzPDErVFtM3uL',
     domain: 'dev-85d746sq.auth0.com', // e.g., https://you.auth0.com/
     audience: 'http://frontend.devinwagner.tech', // e.g., http://localhost:3001
-    redirect: 'http://frontend.devinwgner.tech/callback',
+    redirect: 'http://frontend.devinwagner.tech/callback',
     scope: 'openid profile email'
   },
   baseUrl: 'http://backend.devinwagner.tech/api/books'
