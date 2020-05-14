2e2bd91edc3de9fdb7962c818c7fd7126c947cf6
Devin Wagner <devinwagner003@gmail.com>
2020-05-13 21:15:16 -0600

    Changing the audience and redirect to see if they work with the domain.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
   auth: {
     clientID: 'fW9r7EF5eQQ8iJCtkxQKzPDErVFtM3uL',
     domain: 'dev-85d746sq.auth0.com', // e.g., https://you.auth0.com/
-    audience: 'http://frontend.devinwagner.tech', // e.g., http://localhost:3001
-    redirect: 'http://frontend.devinwagner.tech/callback',
+    audience: 'http://devinwagner.tech', // e.g., http://localhost:3001
+    redirect: 'http://devinwagner.tech/callback',
     scope: 'openid profile email'
   },
   baseUrl: 'http://backend.devinwagner.tech/api/books'
